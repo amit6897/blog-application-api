0ef287e539756c9a8a7f78b74c18e9c44aca748f
amit6897 <amit.jangra21@outlook.com>
2022-09-02 23:08:51 +0530

    update commit

---
+++ src/main/java/com/blogging/security/CustomUserDetailService.java
@@ -0,0 +1,23 @@
+package com.blogging.security;
+
+import com.blogging.entities.User;
+import com.blogging.exceptions.ResourceNotFoundException;
+import com.blogging.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // loading user from db by username
+        User user = this.userRepository.findByEmail(username).orElseThrow(() ->
+                new ResourceNotFoundException("User", "Email id" + username, 0L));
+        return user;
+    }
+}
